9b286a69cf4c7588a4135a366bfb62bcfc9fa5b5
faustinoloeza <fausto_zair@hotmail.com>
2022-03-17 08:08:16 -0500

    all

---
+++ pages/components/navi.js
@@ -0,0 +1,176 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+//import { XIcon } from '@heroicons/react/outline'
+
+const products = [
+    {
+      id: 1,
+      name: 'Mustang',
+      href: '#',
+      imageSrc: 'https://m.media-amazon.com/images/I/51F9eK8B6DS._AC_SY606_.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '35',
+      maker: 'Ford',
+      quantity: 1,
+    },
+    {
+      id: 1,
+      name: 'Mustang',
+      href: '#',
+      imageSrc: 'https://m.media-amazon.com/images/I/51F9eK8B6DS._AC_SY606_.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '35',
+      maker: 'Ford',
+      quantity: 1,
+    },
+    // More products...
+  ]
+
+  export default function Navi() {
+    const [open, setOpen] = useState(true)
+  
+    return (
+      <nav id="header" className="w-full z-30 top-0 py-1">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
+
+          <label for="menu-toggle" className="cursor-pointer md:hidden block">
+              <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                  <title>menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+          </label>
+          <input className="hidden" type="checkbox" id="menu-toggle" />
+
+          <div className="order-1 md:order-2">
+              <img src="https://via.placeholder.com/200x50"></img>
+          </div>
+
+          <div className="order-2 md:order-3 flex items-center" id="nav-content">
+              <a className="pl-3 inline-block no-underline hover:text-black" href="#" onClick={() => setOpen(true)}>
+                  <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
+                      <circle cx="10.5" cy="18.5" r="1.5" />
+                      <circle cx="17.5" cy="18.5" r="1.5" />
+                  </svg>
+              </a>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
+          <div className="absolute inset-0 overflow-hidden">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+  
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <div className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-medium text-gray-900"> Shopping cart </Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            X
+                          </button>
+                        </div>
+                      </div>
+  
+                      <div className="mt-8">
+                        <div className="flow-root">
+                          <ul role="list" className="-my-6 divide-y divide-gray-200">
+                            {products.map((product) => (
+                              <li key={product.id} className="flex py-6">
+                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                  <img
+                                    src={product.imageSrc}
+                                    alt=""
+                                    className="h-full w-full object-cover object-center"
+                                  />
+                                </div>
+  
+                                <div className="ml-4 flex flex-1 flex-col">
+                                  <div>
+                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                      <h3>
+                                        <a href=""> {product.name} </a>
+                                      </h3>
+                                      <p className="ml-4">{product.price}</p>
+                                    </div>
+                                  </div>
+                                  <div className="flex flex-1 items-end justify-between text-sm">
+                                    <p className="text-gray-500">Cantidad {product.quantity}</p>
+  
+                                    <div className="flex">
+                                      <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Eliminar
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+  
+                    <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                      <div className="flex justify-between text-base font-medium text-gray-900">
+                        <p>Subtotal</p>
+                        <p>{products.reduce((a, b) => +a + +b.price, 0)}</p>
+                      </div>
+                      <div className="mt-6">
+                        <a
+                          href="#"
+                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                        >
+                          Checkout
+                        </a>
+                      </div>
+                      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                        <p>
+                          or{' '}
+                          <button
+                            type="button"
+                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                            onClick={() => setOpen(false)}
+                          >
+                            Continue Shopping<span aria-hidden="true"> &rarr;</span>
+                          </button>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      
+      </div>
+      </div>
+  </nav>
+    )
+  }
